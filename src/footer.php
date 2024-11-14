310cfd1ae106969e1c7f3058f423a33084ff7bd4
Santiago <santiago2004gjq@gmail.com>
2024-11-14 16:18:28 -0500

    Proyectos Arduino
    Proyectos Arduino

---
+++ src/footer.php
@@ -0,0 +1,12 @@
+<div class="container-footer">	
+    <footer>
+        <div class="logo-footer">
+            <img src="img/imagen.png" alt="">
+        </div>
+
+        <hr>
+        <p class="fecha"></p>
+        <p class="tiempo"></p>
+        <h4>© 2024 - Titulo - Todos los Derechos Reservados</h4>
+    </footer>
+</div>
---
+++ src/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <link rel="icon" type="image/png" href="img/imagen.png">
+    <title>Proyectos - IED Rincon Santo</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/principal.css">
+    <link rel="stylesheet" href="css/carousel.css">
+    <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/articulos.css">
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <script src="js/script.js"></script>
+    <script src="reloj.js"></script>
+</head>
